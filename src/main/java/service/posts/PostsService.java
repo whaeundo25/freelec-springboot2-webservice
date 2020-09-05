9c24cf40c4f53948191c34f40c12e05453d81611
whaeundo25 <doeun536@gmail.com>
2020-09-05 19:58:13 +0900

    3단원 진행

---
+++ src/main/java/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package service.posts;
+
+import com.whaeun25.book.springboot.domain.posts.PostsRepository;
+import com.whaeun25.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+}
